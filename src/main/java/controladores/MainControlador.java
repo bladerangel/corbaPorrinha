f9a5c662821155863e0c47d8a19a4f0c0c71ad68
bladerangel <bladerangel10@gmail.com>
2017-06-13 13:37:49 -0300

    first commit

---
+++ src/main/java/controladores/MainControlador.java
@@ -0,0 +1,22 @@
+package controladores;
+
+import javafx.fxml.Initializable;
+import servicos.MainServico;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainControlador implements Initializable {
+
+    private MainServico mainServico;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    //o jogador clica no para sair do jogo
+    public void sairPartida() {
+        //mainServico.getTabuleiroEnviarPacoteServico().enviarPacoteSairPartida();
+    }
+}
